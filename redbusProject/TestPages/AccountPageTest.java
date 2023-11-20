7a107c7a0adb0dc2d8c796f60e0c2d73a4057560
sameena shaik <148947028+820888@users.noreply.github.com>
2023-11-20 09:46:33 +0530

    Add files via upload

---
+++ redbusProject/TestPages/AccountPageTest.java
@@ -0,0 +1,35 @@
+package redbusProject.TestPages;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import redbusProject.Base.TestBase;
+import redbusProject.pages.Homepage;
+public class AccountPageTest extends TestBase {
+
+	Homepage hp;
+
+	@BeforeClass
+	public void OpenApp() {
+		openBrowser("Chrome");
+		hp = new Homepage(driver);
+	}
+
+	@Test
+	public void clickOnSignUpPage() throws InterruptedException {
+
+		Thread.sleep(2000);
+		hp.clickOnMyAccount();
+		Thread.sleep(5000);
+		hp.clickOnSignup();
+		Thread.sleep(5000);
+		
+
+	}
+	@AfterClass
+	public void CloseApp() {
+		driver.quit();
+	}
+
+}
